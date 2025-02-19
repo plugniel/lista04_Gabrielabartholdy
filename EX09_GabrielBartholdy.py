9cf658b91d93648b4ade34d69ba5e4bf5db150e9
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-02-18 22:06:53 -0300

    Update EX09_GabrielBartholdy.py

--- EX09_GabrielBartholdy.py
+++ EX09_GabrielBartholdy.py
@@ -2,9 +2,9 @@ dire = input("Qual a direção que deseja apontar?(C para cima, A para baixo) ")
 
 if dire == 'a':
     num = int(input("qual o numero que deseja? (Abaixo de 20)"))
-    for i in range(20,num ,-1):
+    for i in range(20,num -1 ,-1):
         print(i)
 elif dire == 'c':
     num = int(input("qual o numero que deseja? (Superior a 1)"))
-    for i in range(num):
+    for i in range(num + 1):
         print(i)
