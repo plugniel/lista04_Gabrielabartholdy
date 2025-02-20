f45675f588c3a877bd9e8653fda405a9acbfe54e
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-02-19 21:31:27 -0300

    Atividade pronto
    certo!

---
+++ EX11_GabrielBartholdy.py
@@ -0,0 +1,12 @@
+tarefas = int (input('Quantas tarefa deseja realizar?'))
+tare = []
+conclu = []
+
+for i in range(tarefas):
+        ss = input("Qual o nome da tarefa?")
+        tare.append(ss)
+        print("")
+        ss1 = input("Esta tarefa esta concluida?")
+        conclu.append(ss1)
+        print('')
+        print( "TAREFA:{} \nCONCLUIDA:{}".format(tare[i],conclu[i]))
---
+++ EX12_GabrielBartholdy.py
@@ -0,0 +1,25 @@
+horario_disp = []
+n = int(input('Quantos horarios deseja adicionar na lista?'))
+
+for i in range(n):
+    horario = input('Digite o horario')
+    horario_disp.append(horario)
+for i in range(len(horario_disp)):
+    print('\nHorarios disponiveis:')
+    for i,horario in  enumerate(horario_disp):
+        print("{}°{}".format(i+1,horario))
+    escolha_h = int(input("escolha o horario pelo numero"))
+    if 1 <= escolha_h <= len(horario_disp):
+        hora_sele = horario_disp[escolha_h - 1]
+        print("Horario de {} foi agendado com sucesso".format(hora_sele))
+        horario_disp.remove(hora_sele)
+    else:
+        print("aValor invalido")
+    if len(hora_sele) == 0:
+        print("Todos os horarios marcados")
+        break
+    conti = input("deseja marcar mais um horario?(s,n)")
+    if conti != 's':
+        print("encerrando...")
+        break
+    
